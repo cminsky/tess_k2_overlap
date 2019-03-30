54ff9814f3c52b62ba7592fd52c247330bc21ded
cminsky <charlotte.minsky@gmail.com>
2019-03-30 17:51:13 -0400

    more transit prediction fiddles

--- MakeLightcurves.py
+++ MakeLightcurves.py
@@ -3,12 +3,14 @@ import lightkurve as lk
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
+from astropy.time import *
+from parameters import *
 print('lk version:',lk.__version__)
 
 # Fetching planet information
 crossmatch_planets = pandas.read_csv('data/crossmatch_planets.csv')
 crossmatch_candidates = pandas.read_csv('data/crossmatch_candidates.csv')
-combined = pandas.concat([crossmatch_planets,crossmatch_candidates],sort=False)
+combined = pandas.concat([crossmatch_planets,crossmatch_candidates],sort=True)
 
 def get_star_name(tic=None,epic=None):
     """Return the name of a host star based on its TIC ID."""
@@ -74,17 +76,43 @@ def make_lc(tic=None,epic=None,hostname=None,mission='tess',verbose=False):
     if verbose: print('Success')    
     return lc
 
-# For pretty plotting
-colors = ['dodgerblue','hotpink','darkmagenta','navy','mediumseagreen']
+def predict_transits(hostname,period=None,t0=None,mission='tess',verbose=False):
 
-def test_colors():
-    """Test the color scheme."""
-    fig, ax = plt.subplots()
-    for c in colors:
-        x = np.random.uniform(-3, 3, size=20)
-        y = np.random.uniform(-3, 3, size=20)
-        ax.scatter(x,y,color=c)
-    plt.show()
+    if period == None or t0 == None:
+        if verbose: print('Fetching planet parameters...')
+        period, t0, dur, depth, impact_param, r_planet, r_star = get_params(hostname)
+
+    if verbose: print('Predicting transits...')
+    now = Time.now().jd
+    latest_transit = t0
+    transits = []
+    while latest_transit < now:
+        transits.append(latest_transit)
+        latest_transit += period 
+
+    if verbose: 'Calculating time window...'
+    if mission.lower() == 'tess':
+        planet_column_name = 'sector'
+        file_name = 'data/times_TESS.csv'
+        time_column_name = 'sector'
+    elif mission.lower() == 'k2':
+        planet_column_name = 'k2_campaign_str'
+        file_name = 'data/times_K2.csv'
+        time_column_name = 'sector'
+
+    index = crossmatch_planets.index[crossmatch_planets['pl_hostname'] == hostname].tolist()
+    sector_campaign = crossmatch_planets[planet_column_name][index].tolist()[0]
+    times = pandas.read_csv(file_name)
+    time_index = times.index[times[time_column_name] == sector_campaign].tolist()
+    start_jd = times['start_jd'][time_index].tolist()[0]
+    end_jd = times['end_jd'][time_index].tolist()[0]
+
+    overlap_transits = [t for t in transits if t > start_jd and t < end_jd]
+    
+    if verbose: print('Success')
+    return overlap_transits
+
+
 
 def plot_lc(lc=None,tic=None,epic=None,hostname=None,mission='tess',transit_dict=None,verbose=False):
     """Plot the light curve of a host star with predicted transit overlaid.
@@ -99,6 +127,8 @@ def plot_lc(lc=None,tic=None,epic=None,hostname=None,mission='tess',transit_dict
     transit_dict -- predicted transits, of the form {'b':[JD1,JD2]}
     verbose -- whether to print status updates (default False)
     """
+    colors = ['dodgerblue','hotpink','darkmagenta','navy','mediumseagreen']
+
     if lc == None:
         lc = make_lc(tic=tic,epic=epic,hostname=hostname,mission=mission,verbose=verbose)
 
